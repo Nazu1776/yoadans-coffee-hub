dcd08a14ca729a14644823d4674a709d88d354f9
Nazrawi Asrat <nasrat2676@gmail.com>
2024-10-31 14:32:40 -0700

    yoadans coffee hub

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <footer>
+            <p>© 2024 YOADAN's Coffee Hub. All rights reserved.</p>
+        </footer>
+    );
+};
+
+export default Footer;
+
