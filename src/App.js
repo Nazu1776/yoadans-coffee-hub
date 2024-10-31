72a82561d939bc1bb38e87d9f76a3167729eab33
Nazrawi Asrat <nasrat2676@gmail.com>
2024-10-31 14:18:24 -0700

    yoadans coffee hub

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Header from './components/Header';
+import ProductList from './components/ProductList';
+import Footer from './components/Footer';
+
+const App = () => {
+    return (
+        <div className="app">
+            <Header />
+            <ProductList />
+            <Footer />
+        </div>
+    );
+};
+
+export default App;
+
