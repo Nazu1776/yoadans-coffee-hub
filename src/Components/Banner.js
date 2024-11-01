9cd0df24f4c5968799f7dc48a28c242eec346bff
Nazrawi Asrat <nasrat2676@gmail.com>
2024-11-01 10:47:31 -0700

    yoadans coffee hub

---
+++ src/Components/Banner.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Banner.css';
+
+function Banner() {
+  return (
+    <div className="banner">
+      <img src="/images/banner-coffee.jpg" alt="Coffee banner" />
+      <h2>Experience the Rich Flavor of Premium Coffee</h2>
+    </div>
+  );
+}
+
+export default Banner;
+
